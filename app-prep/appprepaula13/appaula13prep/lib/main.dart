081cf96c999dbb76b12b6d58e1dc54432781643f
danielvieira95 <engdanielvieira95@gmail.com>
2025-10-28 16:29:55 -0300

    app prep aula 13

--- app-prep/appprepaula13/appaula13prep/lib/main.dart
+++ app-prep/appprepaula13/appaula13prep/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:appaula13prep/screens/scanner_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -17,7 +18,7 @@ class QRReaderApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.indigo,
       ),
-      home: QRReaderApp(),
+      home: QrScannerPage(),
     );
   }
 }
